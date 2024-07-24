4f6d6eb98b738991769f2c14e770e538621c8f33
4hmedhabib <info.ahmed.developer@gmail.com>
2024-07-24 10:50:07 +0300

    update framework

--- internal/ports/framework_left.go
+++ internal/ports/framework_left.go
@@ -1,9 +1,14 @@
 package ports
 
+import (
+	"context"
+	"hex_arch_go/internal/adapters/framework/left/grpc/proto/pb"
+)
+
 type GRPCPort interface {
 	Run()
-	GetAddition()
-	GetSubtraction()
-	GetMultiplication()
-	GetDivision()
+	GetAddition(ctx context.Context, req *pb.OperationParams) (*pb.Answer, error)
+	GetSubtraction(ctx context.Context, req *pb.OperationParams) (*pb.Answer, error)
+	GetMultiplication(ctx context.Context, req *pb.OperationParams) (*pb.Answer, error)
+	GetDivision(ctx context.Context, req *pb.OperationParams) (*pb.Answer, error)
 }
